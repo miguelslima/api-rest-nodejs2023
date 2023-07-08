641f0ef2120dd79d2abf63176fb08aa1093c1c59
Miguel Lima <miguelsousalima@hotmail.com>
2023-07-07 23:16:01 -0300

    Projeto Api Rest Node

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+import crypto from "node:crypto";
+import { knex } from "./database";
+import { env } from "./env";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const transaction = await knex("transactions").select("*");
+  return transaction;
+});
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });
